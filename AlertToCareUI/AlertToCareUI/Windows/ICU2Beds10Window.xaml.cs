2aadef0940daed972a0e92aa3747f00e0cebd64c
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-11-02 12:16:20 +0530

    Add repo

---
+++ AlertToCareUI/AlertToCareUI/Windows/ICU2Beds10Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AlertToCareUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for ICU2Beds10Window.xaml
+    /// </summary>
+    public partial class ICU2Beds10Window : Window
+    {
+        public ICU2Beds10Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AlertToCareUI/AlertToCareUI/Windows/ICU2Beds12Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AlertToCareUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for ICU2Beds12Window.xaml
+    /// </summary>
+    public partial class ICU2Beds12Window : Window
+    {
+        public ICU2Beds12Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
